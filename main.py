b6d3e1222b3fb5252206f82c01da1d415f84ff41
Carlo Bolis <carlov.ps4@gmail.com>
2026-06-20 11:16:06 +0200

    aggiunta immagini + render_template

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/')
 def home_page():
-    return "Hello world!"
+    return render_template('homepage.html')
+
+@app.route('/sezione1')
+def sezione1():
+    return render_template('first.html')
+
+@app.route('/sezione2')
+def sezione2():
+    return render_template('second.html')
+   
+
+
+
+
 
 app.run(debug=True)
